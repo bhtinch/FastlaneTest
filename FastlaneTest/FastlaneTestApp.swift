aee2ad7a89b8a1fe16ceb8a3e90feb340897a819
BenTincher <benjamintincher@pgatourhq.com>
2024-02-02 07:31:59 -0500

    Initial Commit

---
+++ FastlaneTest/FastlaneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneTestApp.swift
+//  FastlaneTest
+//
+//  Created by Benjamin Tincher on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
